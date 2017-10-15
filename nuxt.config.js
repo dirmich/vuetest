570fa559c60044cfe5761bf3888bf3bb1ef2f93b
David, Shin <dirmich@gmail.com>
2017-10-15 17:24:23 +0900

    1

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'vuetest',
+    title: 'Welcome to Ontime-Study',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
